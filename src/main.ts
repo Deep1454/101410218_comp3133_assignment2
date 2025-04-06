eaeb21dd95356d6af7f088e6124899da51b60ef1
deeplakhani <101410218@georgebrown.ca>
2025-04-06 01:47:12 -0400

    preparing for deploy

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,7 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './app/components/login/login.component';
-import { SignupComponent } from './app/components/signup/signup.component';
-import { EmployeeListComponent } from './app/components/employee-list/employee-list.component';
-import { EmployeeAddComponent } from './app/components/employee-add/employee-add.component';
-import { EmployeeDetailsComponent } from './app/components/employee-details/employee-details.component';
-import { EmployeeUpdateComponent } from './app/components/employee-update/employee-update.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-export const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'employees', component: EmployeeListComponent },
-  { path: 'employee/add', component: EmployeeAddComponent },
-  { path: 'employee/:id', component: EmployeeDetailsComponent },
-  { path: 'employee/update/:id', component: EmployeeUpdateComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' },
-];
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
